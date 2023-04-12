76b731fec68421f79883b3bdc38477e689ded9d8
gustavwiese <gusse12345@gmail.com>
2023-04-12 08:42:27 +0200

    asdf

--- script.js
+++ script.js
@@ -11,25 +11,37 @@ function start() {
   for (let i = 0; i < 40; i++) createBars(i);
 }
 
-function getNumberofCustomers() {
+function getNumberOfCustomers() {
   return Math.floor(Math.random() * 32);
 }
 
 function createBars(num) {
+  console.log(`createBars`);
   let barElement = /*html*/ `
-<div id="bar${num}" class="bar"></div>
-
+    <div id="bar${num}" class="bar"></div>
 `;
   document.querySelector("#ui").insertAdjacentHTML("beforeend", barElement);
 }
 
 function countList() {
-  const queueSize = getNumberofCustomers();
+  const queueSize = getNumberOfCustomers();
   numberList.push(queueSize);
   if (numberList.length > 40) {
     numberList.shift();
   }
+  console.log(numberList);
   styleAllBars();
 }
 
+function styleBar(num, newHeight) {
+  let bar = document.querySelector("#bar" + num);
+  bar.style.height = newHeight * 2 + "%";
+  let compensatedTop = defaultTop + defaultHeight - newHeight * 2;
+  bar.style.top = compensatedTop + "%";
+}
 
+function styleAllBars() {
+  for (let i = 0; i < numberList.length; i++) {
+    styleBar(i, numberList[i]);
+  }
+}
